350aab287914916e7183bf86c07def2f72ae8962
TimofeyImaev <135110212+TimofeyImaev@users.noreply.github.com>
2023-10-07 09:52:19 +0500

    Deleted useful branches

--- src/test/java/edu/hw1/SampleTest.java
+++
@@ -1,9 +0,0 @@
-package edu.hw1;
-
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-
-public class SampleTest {
-
-}
