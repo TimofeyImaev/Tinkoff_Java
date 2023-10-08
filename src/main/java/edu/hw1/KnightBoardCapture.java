06662fa3ea4d9c58ba1c9f2d4ae4bfb08b8ad7d4
TimofeyImaev <135110212+TimofeyImaev@users.noreply.github.com>
2023-10-08 13:03:05 +0500

    Bugs with checker fixed

--- src/main/java/edu/hw1/KnightBoardCapture.java
+++ src/main/java/edu/hw1/KnightBoardCapture.java
@@ -2,7 +2,6 @@ package edu.hw1;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import java.security.KeyPair;
 
 public class KnightBoardCapture {
     private final static Logger LOGGER = LogManager.getLogger();
@@ -11,25 +10,28 @@ public class KnightBoardCapture {
 
     }
 
-    public static boolean knightBoardCapture(int[][] arr){
-        Pair<Integer, Integer>[] coords = new Pair[]{
+    public static boolean knightBoardCapture(int[][] arr) {
+        final int boardSideSize = 8;
+        final int minusTwo = -2;
+        Pair<Integer, Integer>[] coords = new Pair[] {
             new Pair<>(2, 1),
             new Pair<>(1, 2),
             new Pair<>(2, -1),
             new Pair<>(-1, 2),
-            new Pair<>(-1, -2),
-            new Pair<>(-2, -1),
-            new Pair<>(-2, 1),
-            new Pair<>(1, -2),
+            new Pair<>(-1, minusTwo),
+            new Pair<>(minusTwo, -1),
+            new Pair<>(minusTwo, 1),
+            new Pair<>(1, minusTwo),
         };
 
-        for (int i = 0; i < 8; ++i) {
-            for (int j = 0; j < 8; ++j) {
+        for (int i = 0; i < boardSideSize; ++i) {
+            for (int j = 0; j < boardSideSize; ++j) {
                 for (var each : coords) {
                     int x = each.first + i;
                     int y = each.second + j;
 
-                    if (x >= 0 && y >= 0 && x < 8 && y < 8 && arr[x][y] == 1 && arr[i][j] == 1) {
+                    if (x >= 0 && y >= 0 && x < boardSideSize && y < boardSideSize
+                        && arr[x][y] == 1 && arr[i][j] == 1) {
                         LOGGER.trace("Knight {}, {} hits {}, {}", i, j, x, y);
                         return false;
                     }
