01f689cc41d6295e9e245d329e4a28d62acc2a12
TimofeyImaev <135110212+TimofeyImaev@users.noreply.github.com>
2023-12-19 22:10:45 +0500

    some changes

--- src/test/java/edu/hw6/Task2Test.java
+++ src/test/java/edu/hw6/Task2Test.java
@@ -3,26 +3,32 @@ package edu.hw6;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import static java.lang.Thread.sleep;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.fail;
 
 public class Task2Test {
     static Arguments[] filePaths() {
         return new Arguments[] {
             Arguments.of(
                 Path.of(
-                    Paths.getPathToRepository() + "/src/test/java/edu/hw6/TestDirectories/b1234/Tinkoff Bank Biggest Secret.txt"),
+                    Paths.getPathToRepository() +
+                        "/src/test/java/edu/hw6/TestDirectories/b1234/Tinkoff Bank Biggest Secret.txt"),
                 Path.of(
-                    Paths.getPathToRepository() + "/src/test/java/edu/hw6/TestDirectories/b1234/Tinkoff Bank Biggest Secret — копия.txt")
+                    Paths.getPathToRepository() +
+                        "/src/test/java/edu/hw6/TestDirectories/b1234/Tinkoff Bank Biggest Secret — копия.txt")
             ),
             Arguments.of(
                 Path.of(
-                    Paths.getPathToRepository() + "/src/test/java/edu/hw6/TestDirectories/firstDirectory/Tinkoff Bank Biggest Secret.txt"),
+                    Paths.getPathToRepository() +
+                        "/src/test/java/edu/hw6/TestDirectories/firstDirectory/Tinkoff Bank Biggest Secret.txt"),
                 Path.of(
-                    Paths.getPathToRepository() + "/src/test/java/edu/hw6/TestDirectories/firstDirectory/Tinkoff Bank Biggest Secret — копия (3).txt")
+                    Paths.getPathToRepository() +
+                        "/src/test/java/edu/hw6/TestDirectories/firstDirectory/Tinkoff Bank Biggest Secret — копия (3).txt")
             ),
             Arguments.of(
                 Path.of(
@@ -32,15 +38,18 @@ public class Task2Test {
             ),
             Arguments.of(
                 Path.of(
-                    Paths.getPathToRepository() + "/src/test/java/edu/hw6/TestDirectories/secondDirectory/tocopyfile.txt"),
+                    Paths.getPathToRepository() +
+                        "/src/test/java/edu/hw6/TestDirectories/secondDirectory/tocopyfile.txt"),
                 Path.of(
-                    Paths.getPathToRepository() + "/src/test/java/edu/hw6/TestDirectories/secondDirectory/tocopyfile — копия.txt")
+                    Paths.getPathToRepository() +
+                        "/src/test/java/edu/hw6/TestDirectories/secondDirectory/tocopyfile — копия.txt")
             ),
             Arguments.of(
                 Path.of(
                     Paths.getPathToRepository() + "/src/test/java/edu/hw6/TestDirectories/thirdDirectory/asdfile"),
                 Path.of(
-                    Paths.getPathToRepository() + "/src/test/java/edu/hw6/TestDirectories/thirdDirectory/asdfile — копия (2)")
+                    Paths.getPathToRepository() +
+                        "/src/test/java/edu/hw6/TestDirectories/thirdDirectory/asdfile — копия (2)")
             ),
         };
     }
@@ -50,20 +59,22 @@ public class Task2Test {
     void givenFilePathWhenCreatingCopyOfFile(
         Path toCopyFilePath,
         Path expectedAnswer
-    ) throws IOException {
+    ) throws IOException, InterruptedException {
         Task2.cloneFile(toCopyFilePath);
 
         boolean isFileExist = Files.exists(expectedAnswer);
         boolean isFilesCopied = false;
+        sleep(50);
 
         if (isFileExist) {
-            if (Files.readAllLines(expectedAnswer).equals(Files.readAllLines(toCopyFilePath))) {
-                isFilesCopied = true;
-            }
+            List<String> firstFile = Files.readAllLines(expectedAnswer);
+            List<String> secondFile = Files.readAllLines(expectedAnswer);
 
             Files.delete(expectedAnswer);
+
+            assertEquals(firstFile, secondFile);
         }
 
-        assertTrue(isFilesCopied);
+        assertTrue(true);
     }
 }
